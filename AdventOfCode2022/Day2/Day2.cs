679eedabe3311ba2309b0a19d81d8e5283870dde
Tyler Harbert <tyler.harbert@rovisys.com>
2022-12-03 00:44:08 -0500

    Day 2 and 3

--- AdventOfCode2022/Day2/Day2.cs
+++ AdventOfCode2022/Day2/Day2.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2022.Day2
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022.Day2
 {
     internal class Day2 : IChallengeDay
     {
@@ -6,12 +8,77 @@
 
         public void RunChallengeOne()
         {
-            Console.WriteLine("Placeholder answer.");
+            int totalScore = 0;
+
+            foreach (string line in File.ReadLines(@".\Day2\challenge-input.txt"))
+            {
+                var opponentPlay = line.First();
+                var myPlay = line.Last();
+
+                var opponentScore = _scores[opponentPlay];
+                var myScore = _scores[myPlay];
+
+                totalScore += myScore;
+
+                if (opponentScore == myScore)
+                {
+                    totalScore += 3;
+                }
+                else if (DecrementScore(myScore) == opponentScore)
+                {
+                    totalScore += 6;
+                }                
+            }
+
+            Console.WriteLine($"The total score would be {totalScore}.");
         }
+
+        private int DecrementScore(int score)
+        {
+            var temp = score - 1;
+
+            return temp == 0 ? 3 : temp;
+        }
+
+        private int IncrementScore(int score)
+        {
+            var temp = score + 1;
+
+            return temp == 4 ? 1 : temp;
+        }
+
+        // Scores
+        private readonly IDictionary<char, int> _scores = new Dictionary<char, int> { { 'A', 1 }, { 'B', 2 }, { 'C', 3 }, { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 } };
 
         public void RunChallengeTwo()
         {
-            Console.WriteLine("Placeholder answer.");
+            int totalScore = 0;
+
+            foreach (string line in File.ReadLines(@".\Day2\challenge-input.txt"))
+            {
+                var opponentPlay = line.First();
+                var decision = line.Last();
+
+                var opponentScore = _scores[opponentPlay];
+                var decisionScore = _scores[decision];
+
+                // Tie
+                if (decisionScore == 2)
+                {
+                    totalScore += 3 + opponentScore;
+                }
+                // Win
+                else if (decisionScore == 3)
+                {
+                    totalScore += 6 + IncrementScore(opponentScore);
+                }
+                else
+                {
+                    totalScore += DecrementScore(opponentScore);
+                }
+            }
+
+            Console.WriteLine($"The total score would be {totalScore}.");
         }
     }
 }
---
+++ AdventOfCode2022/Day3/Day3.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode2022.Day3
+{
+    internal sealed class Day3 : IChallengeDay
+    {
+        public int DayNumber => 3;
+
+        public void RunChallengeOne()
+        {
+            int runningTotal = 0;
+
+            foreach (string line in File.ReadLines(@".\Day3\challenge-input.txt"))
+            {
+                var compartmentLength = line.Length / 2;
+
+                var compartmentOne = line.Substring(0, compartmentLength);
+                var compartmentTwo = line.Substring(compartmentLength, compartmentLength);
+
+                var charVal = compartmentOne.Intersect(compartmentTwo).FirstOrDefault();
+
+                var priority = char.IsLower(charVal) ? (int)charVal - 96 : (int)charVal - 38;
+
+                runningTotal += priority;
+            }
+
+            Console.WriteLine($"The sum of the priorites is {runningTotal}.");
+        }
+
+        public void RunChallengeTwo()
+        {
+            int runningTotal = 0;
+
+            var lines = File.ReadLines(@".\Day3\challenge-input.txt").ToList();
+            int pageLines = 3;
+
+            var pageCount = lines.Count() / pageLines;
+            var pageNumber =  0;
+
+            while (pageNumber < pageCount)
+            {
+                var groupLines = lines.Skip(pageNumber * pageLines).Take(pageLines);
+
+                var charVal = InterativeIntersect(groupLines).FirstOrDefault();
+
+                var priority = char.IsLower(charVal) ? (int)charVal - 96 : (int)charVal - 38;
+
+                runningTotal += priority;
+
+                pageNumber++;
+            }
+
+            Console.WriteLine($"The sum of the priorites is {runningTotal}.");
+        }
+
+        private IEnumerable<char> InterativeIntersect(IEnumerable<string> values)
+        {
+            var enumerator = values.GetEnumerator();
+            enumerator.MoveNext();
+
+            IEnumerable<char> result = enumerator.Current;
+
+            while (enumerator.MoveNext())
+            {
+                result = result.Intersect(enumerator.Current);
+            }
+
+            return result;
+        }
+    }
+}
