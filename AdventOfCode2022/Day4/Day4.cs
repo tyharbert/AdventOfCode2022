9817266b46123e00707301b42c51b35fa3544801
Tyler Harbert <tyler.harbert@rovisys.com>
2022-12-04 08:32:28 -0500

    Day 4 cleanup

--- AdventOfCode2022/Day4/Day4.cs
+++ AdventOfCode2022/Day4/Day4.cs
@@ -10,22 +10,18 @@
 
             foreach (string line in File.ReadLines(@".\Day4\challenge-input.txt"))
             {
-                var pairValues = line.Split('-', ',')
-                    .Select(v => int.TryParse(v, out int result) ? result : throw new NotSupportedException("The input row must be '0-0,0-0'."))
-                    .ToArray();
+                int[] pairValues = ParsePairValues(line);
 
-                if (pairValues.Length < 4)
-                {
-                    throw new NotSupportedException("The input row must be '0-0,0-0'.");
-                }
+                var partner1 = new Range(pairValues[0], pairValues[1]);
+                var partner2 = new Range(pairValues[2], pairValues[3]);
 
                 // Partner 1 contains 2
-                if (pairValues[0] <= pairValues[2] && pairValues[1] >= pairValues[3])
+                if (partner1.Start <= partner2.Start && partner1.End >= partner2.End)
                 {
                     fullAssignemntOverlapCount++;
                 }
                 // Partner 2 contains 1
-                else if (pairValues[2] <= pairValues[0] && pairValues[3] >= pairValues[1])
+                else if (partner2.Start <= partner1.Start && partner2.End >= partner1.End)
                 {
                     fullAssignemntOverlapCount++;
                 }
@@ -40,23 +36,45 @@
 
             foreach (string line in File.ReadLines(@".\Day4\challenge-input.txt"))
             {
-                var pairValues = line.Split('-', ',')
-                    .Select(v => int.TryParse(v, out int result) ? result : throw new NotSupportedException("The input row must be '0-0,0-0'."))
-                    .ToArray();
+                int[] pairValues = ParsePairValues(line);
 
-                if (pairValues.Length < 4)
-                {
-                    throw new NotSupportedException("The input row must be '0-0,0-0'.");
-                }
+                var partner1 = new Range(pairValues[0], pairValues[1]);
+                var partner2 = new Range(pairValues[2], pairValues[3]);
 
                 // Any assignment overlap
-                if (pairValues[0] <= pairValues[3] && pairValues[1] >= pairValues[2])
+                if (partner1.Start <= partner2.End && partner1.End >= partner2.Start)
                 {
                     anyAssignemntOverlapCount++;
                 }
             }
 
             Console.WriteLine($"The number of pairs with any assignment overlap is {anyAssignemntOverlapCount}.");
+        }
+
+        private static int[] ParsePairValues(string line)
+        {
+            var pairValues = line.Split('-', ',')
+                .Select(v => int.TryParse(v, out int result) ? result : throw new NotSupportedException("The input row must be '0-0,0-0'."))
+                .ToArray();
+
+            if (pairValues.Length < 4)
+            {
+                throw new NotSupportedException("The input row must be '0-0,0-0'.");
+            }
+
+            return pairValues;
+        }
+
+        private sealed class Range
+        {
+            public Range(int start, int end)
+            {
+                Start = start;
+                End = end;
+            }
+
+            public int Start { get; }
+            public int End { get; }
         }
     }
 }
