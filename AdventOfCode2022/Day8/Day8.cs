e2772b80ae83cca51f1d704f66d84567fa0557d4
Tyler Harbert <tyler.harbert@rovisys.com>
2022-12-09 22:59:22 -0500

    Day8 and Day9

---
+++ AdventOfCode2022/Day8/Day8.cs
@@ -0,0 +1,258 @@
+﻿namespace AdventOfCode2022.Day8
+{
+    internal sealed class Day8 : IChallengeDay
+    {
+        public int DayNumber => 8;
+
+        public void RunChallengeOne()
+        {
+            var trees = ParseTrees();
+
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            var visible = new bool[rowCount - 2, colCount - 2];
+
+            CheckMiddleVisibilityTop(trees, visible);
+            CheckMiddleVisibilityBottom(trees, visible);
+            CheckMiddleVisibilityLeft(trees, visible);
+            CheckMiddleVisibilityRight(trees, visible);
+
+            int count = CountMiddleVisibility(visible);
+
+            // Add exterior visibility
+            count += (rowCount * 2) + (colCount * 2) - 4;
+
+            Console.WriteLine($"The count of visible trees is {count}.");
+        }
+
+        public void RunChallengeTwo()
+        {
+            var trees = ParseTrees();
+
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            int highScore = 0;
+
+            for (var row = 1; row < rowCount - 1; row++)
+            {
+                for (var col = 1; col < colCount - 1; col++)
+                {
+                    int score = GetScoreUp(trees, row, col);
+                    score *= GetScoreDown(trees, row, col);
+                    score *= GetScoreLeft(trees, row, col);
+                    score *= GetScoreRight(trees, row, col);
+
+                    if (score > highScore)
+                    {
+                        highScore = score;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The highest scenic score is {highScore}.");
+        }
+
+        private int GetScoreUp(int[,] trees, int rowStart, int col)
+        {
+            var compareValue = trees[rowStart, col];
+            var score = 0;
+
+            for (var row = rowStart - 1; row >= 0; row--)
+            {
+                score++;
+
+                if (trees[row, col] >= compareValue)
+                {
+                    break;
+                }
+            }
+
+            return score;
+        }
+
+        private int GetScoreDown(int[,] trees, int rowStart, int col)
+        {
+            var compareValue = trees[rowStart, col];
+            var score = 0;
+
+            for (var row = rowStart + 1; row < trees.GetLength(0); row++)
+            {
+                score++;
+
+                if (trees[row, col] >= compareValue)
+                {
+                    break;
+                }
+            }
+
+            return score;
+        }
+
+        private int GetScoreLeft(int[,] trees, int row, int colStart)
+        {
+            var compareValue = trees[row, colStart];
+            var score = 0;
+
+            for (var col = colStart - 1; col >= 0; col--)
+            {
+                score++;
+
+                if (trees[row, col] >= compareValue)
+                {
+                    break;
+                }
+            }
+
+            return score;
+        }
+
+        private int GetScoreRight(int[,] trees, int row, int colStart)
+        {
+            var compareValue = trees[row, colStart];
+            var score = 0;
+
+            for (var col = colStart + 1; col < trees.GetLength(1); col++)
+            {
+                score++;
+
+                if (trees[row, col] >= compareValue)
+                {
+                    break;
+                }
+            }
+
+            return score;
+        }
+
+        private static int CountMiddleVisibility(bool[,] visible)
+        {
+            int count = 0;
+            for (int i = 0; i < visible.GetLength(0); i++)
+            {
+                for (int j = 0; j < visible.GetLength(1); j++)
+                {
+                    if (visible[i, j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private static void CheckMiddleVisibilityTop(int[,] trees, bool[,] visible)
+        {
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            int min;
+
+            for (int col = 1; col < colCount - 1; col++)
+            {
+                min = trees[0, col];
+
+                for (int row = 1; row < rowCount - 1; row++)
+                {
+                    if (trees[row, col] > min)
+                    {
+                        min = trees[row, col];
+                        visible[row - 1, col - 1] = true;
+                    }
+                }
+            }
+        }
+
+        private static void CheckMiddleVisibilityBottom(int[,] trees, bool[,] visible)
+        {
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            int min;
+
+            for (int col = 1; col < colCount - 1; col++)
+            {
+                min = trees[rowCount - 1, col];
+
+                for (int row = rowCount - 2; row >= 1; row--)
+                {
+                    if (trees[row, col] > min)
+                    {
+                        min = trees[row, col];
+                        visible[row - 1, col - 1] = true;
+                    }
+                }
+            }
+        }
+
+        private static void CheckMiddleVisibilityLeft(int[,] trees, bool[,] visible)
+        {
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            int min;
+
+            for (int row = 1; row < rowCount - 1; row++)
+            {
+                min = trees[row, 0];
+
+                for (int col = 1; col < colCount - 1; col++)
+                {
+                    if (trees[row, col] > min)
+                    {
+                        min = trees[row, col];
+                        visible[row - 1, col - 1] = true;
+                    }
+                }
+            }
+        }
+
+        private static void CheckMiddleVisibilityRight(int[,] trees, bool[,] visible)
+        {
+            var rowCount = trees.GetLength(0);
+            var colCount = trees.GetLength(1);
+
+            int min;
+
+            for (int row = 1; row < rowCount - 1; row++)
+            {
+                min = trees[row, colCount - 1];
+
+                for (int col = colCount - 2; col >= 1; col--)
+                {
+                    if (trees[row, col] > min)
+                    {
+                        min = trees[row, col];
+                        visible[row - 1, col - 1] = true;
+                    }
+                }
+            }
+        }
+
+        private static int[,] ParseTrees()
+        {
+            var lines = File.ReadLines(@".\Day8\challenge-input.txt").ToList();
+
+            var rowCount = lines.Count;
+            var colCount = lines.First().Length;
+
+            var trees = new int[rowCount, colCount];
+
+            for (var row = 0; row < rowCount; row++)
+            {
+                var rowValues = lines[row]
+                        .Select(c => (int)char.GetNumericValue(c))
+                        .ToArray();
+
+                for (var col = 0; col < colCount; col++)
+                {
+                    trees[row, col] = rowValues[col];
+                }
+            }
+
+            return trees;
+        }
+    }
+}
---
+++ AdventOfCode2022/Day9/Day9.cs
@@ -0,0 +1,215 @@
+﻿namespace AdventOfCode2022.Day9
+{
+    internal sealed class Day9 : IChallengeDay
+    {
+        public int DayNumber => 9;
+
+        public void RunChallengeOne()
+        {
+            HashSet<string> tailHistory = new HashSet<string>();
+
+            Index h = new Index { X = 0, Y = 0 };
+            Index t = new Index { X = 0, Y = 0 };
+
+            foreach (var line in File.ReadLines(@".\Day9\challenge-input.txt"))
+            {
+                var rule = line.Split(' ')
+                    .ToArray();
+
+                var moveCommand = rule[0].FirstOrDefault();
+                var moveAmount = int.Parse(rule[1]);
+
+                for (var i = 0; i < moveAmount; i++)
+                {
+
+                    switch (moveCommand)
+                    {
+                        case 'U':
+                            MoveUp(h);
+                            break;
+                        case 'D':
+                            MoveDown(h);
+                            break;
+                        case 'L':
+                            MoveLeft(h);
+                            break;
+                        case 'R':
+                            MoveRight(h);
+                            break;
+                        default:
+                            throw new NotSupportedException();
+                    }
+
+                    FollowTheLeader(h, t);
+                    MarkTailWasHere(tailHistory, t);
+                }
+            }
+
+            Console.WriteLine($"The key was in this many locations: {tailHistory.Count()}");
+        }
+
+        public void RunChallengeTwo()
+        {
+            HashSet<string> tailHistory = new HashSet<string>();
+
+            Index h = new Index { X = 0, Y = 0 };
+            Index[] tailKnots = Enumerable.Repeat(0, 9).Select(x => new Index()).ToArray();
+
+            foreach (var line in File.ReadLines(@".\Day9\challenge-input.txt"))
+            {
+                var rule = line.Split(' ')
+                    .ToArray();
+
+                var moveCommand = rule[0].FirstOrDefault();
+                var moveAmount = int.Parse(rule[1]);
+
+                for (var i = 0; i < moveAmount; i++)
+                {
+
+                    switch (moveCommand)
+                    {
+                        case 'U':
+                            MoveUp(h);
+                            break;
+                        case 'D':
+                            MoveDown(h);
+                            break;
+                        case 'L':
+                            MoveLeft(h);
+                            break;
+                        case 'R':
+                            MoveRight(h);
+                            break;
+                        default:
+                            throw new NotSupportedException();
+                    }
+
+                    FollowTheLeader(h, tailKnots.First());
+
+                    for (int k = 1; k < tailKnots.Length; k++)
+                    {
+                        FollowTheLeader(tailKnots[k - 1], tailKnots[k]);
+                    }
+
+                    MarkTailWasHere(tailHistory, tailKnots.Last());
+                }
+            }
+
+            Console.WriteLine($"The key was in this many locations: {tailHistory.Count()}");
+        }
+
+        private void FollowTheLeader(Index leader, Index follower)
+        {
+            int changeX = 0;
+            int changeY = 0;
+
+            if (leader.X == follower.X)
+            {
+                changeY = leader.Y - follower.Y;
+            }
+            else if (leader.Y == follower.Y)
+            {
+                changeX = leader.X - follower.X;
+            }
+            else
+            {
+                changeX = leader.X - follower.X;
+                changeY = leader.Y - follower.Y;
+            }
+
+            
+            if (Math.Abs(changeX) == 2)
+            {
+                // Move right
+                if (changeX > 0)
+                {
+                    MoveRight(follower);
+                }
+                // Move left
+                else if (changeX < 0)
+                {
+                    MoveLeft(follower);
+                }
+
+                // Move up (diagonal)
+                if (changeY > 0)
+                {
+                    MoveUp(follower);
+                }
+                // Move down (diagonal)
+                else if (changeY < 0)
+                {
+                    MoveDown(follower);
+                }
+            }
+            else if(Math.Abs(changeY) == 2)
+            {
+                // Move up
+                if (changeY > 0)
+                {
+                    MoveUp(follower);
+                }
+                // Move down
+                else if (changeY < 0)
+                {
+                    MoveDown(follower);
+                }
+
+                // Move right (diagonal)
+                if (changeX > 0)
+                {
+                    MoveRight(follower);
+                }
+                // Move left (diagonal)
+                else if (changeX < 0)
+                {
+                    MoveLeft(follower);
+                }
+            }
+        }
+
+        private Index MoveRight(Index index)
+        {
+            index.X += 1;
+
+            return index;
+        }
+
+        private Index MoveLeft(Index index)
+        {
+            index.X -= 1;
+
+            return index;
+        }
+
+        private Index MoveUp(Index index)
+        {
+            index.Y += 1;
+
+            return index;
+        }
+
+        private Index MoveDown(Index index)
+        {
+            index.Y -= 1;
+
+            return index;
+        }
+
+        private void MarkTailWasHere(HashSet<string> tailHistory, Index tail)
+        {
+            var key = $"{tail.X}{tail.Y}";
+
+            if (!tailHistory.Contains(key))
+            {
+                tailHistory.Add(key);
+            }
+        }
+
+        private sealed class Index
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+    }
+}
